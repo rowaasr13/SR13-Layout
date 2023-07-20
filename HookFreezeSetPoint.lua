9f07a98714afa7feaee420f1574b3178aa37b85a
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2023-07-21 01:18:09 +0300

    additionally call hook once to ensure freeze is applied right when HookFreezeSetPoint is called

--- HookFreezeSetPoint.lua
+++ HookFreezeSetPoint.lua
@@ -3,6 +3,7 @@ local a_name, a_env = ...
 function a_env.HookFreezeSetPoint(frame, hook_func)
    local original_setpoint = frame.SetPoint
 
+   hook_func(frame, original_setpoint)
    hooksecurefunc(frame, "SetPoint", function(frame, ...)
       hook_func(frame, original_setpoint, ...)
    end)
